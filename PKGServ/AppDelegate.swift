294d74426674a3f643c6fcbfdf5ea150567c5ee7
FivePixels <o5pxels@gmail.com>
2019-01-07 15:44:44 -0600

    Initial Commit

---
+++ PKGServ/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PKGServ
+//
+//  Created by Dylan Bolger on 1/7/19.
+//  Copyright © 2019 Dylan Bolger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PKGServ/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PKGServ
+//
+//  Created by Dylan Bolger on 1/7/19.
+//  Copyright © 2019 Dylan Bolger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
