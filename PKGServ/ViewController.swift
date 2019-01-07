2c2bc9797b19ca4ef6643409add976df1006add1
FivePixels <o5pxels@gmail.com>
2019-01-07 15:59:33 -0600

    Added Pods, a bunch of empty buttons. etc.

--- PKGServ/ViewController.swift
+++ PKGServ/ViewController.swift
@@ -10,6 +10,27 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var contentsView: NSTableView!
+    @IBOutlet weak var statusField: NSTextField!
+    @IBOutlet weak var pathField: NSTextField!
+    @IBOutlet weak var localAddressField: NSTextField!
+    @IBOutlet weak var portField: NSTextField!
+    @IBOutlet weak var hostedTextField: NSTextField!
+    
+    @IBAction func networkPrefsPressed(_ sender: NSButton) {
+    }
+    @IBAction func helpButtonPressed(_ sender: NSButton) {
+    }
+    @IBAction func makeListButtonPressed(_ sender: NSButton) {
+    }
+    @IBAction func folderButtonPressed(_ sender: NSButton) {
+    }
+    @IBAction func changePortButtonPressed(_ sender: NSButton) {
+    }
+    @IBAction func serverButtonPressed(_ sender: NSButton) {
+    }
+    @IBAction func refreshButtonPressed(_ sender: NSButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
---
+++ Podfile
@@ -0,0 +1,12 @@
+project 'PKGServ.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+platform :osx, '10.12'
+
+target 'PKGServ' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'Swifter', '~> 1.4.5'
+
+end
